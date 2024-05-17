e0657670c5e07b0fd0742e1eb924c756b3aa4fb2
Alyssa Wang <157160100+Alyssa-Wang12@users.noreply.github.com>
2024-05-17 11:39:24 -0600

    Add files via upload

---
+++ bipTheClown/bipTheClown/bipTheClownApp.swift
@@ -0,0 +1,17 @@
+//
+//  bipTheClownApp.swift
+//  bipTheClown
+//
+//  Created by Wang, Alyssa on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct bipTheClownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
